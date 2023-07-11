6edf11ac67a91666724b50d980c54abe7fa1e37a
uxmoon <uxmoon@gmail.com>
2023-07-11 12:56:53 -0300

    Add page and subpage

---
+++ app/about/page.jsx
@@ -0,0 +1,15 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus enim
+        eveniet reiciendis ex exercitationem odit, qui excepturi! Rem, repellat
+        ut voluptates aliquid consequuntur maxime ipsa, natus, quam voluptatibus
+        porro quod?
+      </p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ app/about/team/page.jsx
@@ -0,0 +1,15 @@
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Our team</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. A sapiente
+        eveniet sequi error ullam dolore consequatur dignissimos voluptas
+        repellendus rem nisi eos, saepe minus atque optio architecto? Quidem,
+        ducimus quo?
+      </p>
+    </div>
+  )
+}
+
+export default TeamPage
